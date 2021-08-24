d6f5cbe0dc6e736863e48c64747daeec3464131b
abhinandanchivate <abhinandanchivate2000@gmail.com>
2021-08-24 17:25:11 +0530

    completed home page designing and modularity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./profile/profile.module').then((n) => n.ProfileModule),
+  },
+  {
+    path: 'post',
+    loadChildren: () => import('./post/post.module').then((p) => p.PostModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/admin-dashboard/admin-dashboard.module').then(
+        (d) => d.AdminDashboardModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
